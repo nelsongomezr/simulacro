6c7286789f76031d5963c6d9aa8162c5620fd0ea
juanE <sebas.enciso121@gmail.com>
2021-10-15 14:49:08 -0500

    Continuacion proceso

--- class/class.php
+++ class/class.php
@@ -1,5 +1,5 @@
 <?php 
-session_start($Usuario);
+
 require("conexion.php");
 
 class simulacro extends Conexion
@@ -18,35 +18,38 @@ class simulacro extends Conexion
 
     public function Crea_eps($v1,$v2,$v3,$v4)
     {
-        $sql="INSERT INTO Eps values '$v1','$v2','$v3','$v4',";
+        $sql="INSERT INTO `eps`(`idEps`, `Nombre`, `Direccion`, `Telefono`, `Perfil_idPerfil`) VALUES ('$v1','$v2','$v3','$v4','2')";
+        $res=$this->conex->query($sql);
+
+        //print_r($sql);
+        //die();
+        
+        echo "<script type='text/javascript'>
+					alert('proceso realizado de manera satisfactoria');
+					window.location='../simulacro/crea_eps.php';
+			</script>";
+        return("$v1,$v2,$v3,$v4");
+    }
+
+    /*public function Crea_cmedi()
+    {
+        $sql="INSERT INTO Cent_Med values /*valores en orden de la tabla ";
         $res=$this->conex->query($sql);
 
         echo "<script type='text/javascript'>
 					alert('proceso realizado de manera satisfactoria');
-					window.location='../Nueva carpeta/simulacro/--/*nombre del archivo del formulario*/--.php';
-			</script>";
-            return("$v1,$v2,$v3,$v4");
-    }
-
-    public function Crea_cmedi()
-    {
-        $sql="INSERT INTO Cent_Med values /*valores en orden de la tabla */";
-        $res=$this->conex->query($sql);
-
-        echo "<script type='text/javascript'>
-					alert('proceso realizado de manera satisfactoria');
-					window.location='../Nueva carpeta/simulacro/--/*nombre del archivo del formulario*/--.php';
+					window.location='../Nueva carpeta/simulacro/--/*nombre del archivo del formulario--.php';
 			</script>";
 
-    }
+    }*/
 
-    public function Valida_ci()
+    /*public function Valida_ci()
     {
-        $sql="SELECT * FROM usuario Where idUsuario='$Usuario'";
+        $sql="SELECT * FROM usuario Where idUsuario=''";
         $res=$this->conex->query($sql);
 
 
-    }
+    }*/
 
 
 
